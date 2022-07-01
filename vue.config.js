a0e5be553e6e2c97e50cfde90744627fdcdf80fe
jayveecruz05 <jayveecruz05@gmail.com>
2022-07-01 14:29:38 +0800

    Added Vue-Template-Babel-Compiler & Dependencies Updates.

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,10 @@ module.exports = {
       args[0].title = 'Template'
       return args
     });
+    config.module.rule('vue').use('vue-loader').tap(options => {
+      options.compiler = require('vue-template-babel-compiler')
+      return options
+    });
   },
   transpileDependencies: [
     'vuetify'
